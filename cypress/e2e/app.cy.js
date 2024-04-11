3eee6d0a894a10b9f0fc0f4bb9811c02ee613e62
Rafael Freitas <oraffaudi@gmail.com>
2024-04-11 20:14:30 -0300

    Added first step in the second scenario

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -77,8 +77,20 @@ describe('Image Registration', () => {
 
 describe('Submitting an image with valid inputs using enter key', () => {
 
+  const input = {
+    title: 'Alien BR',
+    url: 'https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg',
+  }    
+  
+  after(() => {
+    cy.clearLocalStorage()
+  })
 
-  it('Given I am on the image registration page', () => {})
+  it('Given I am on the image registration page', () => {
+    cy.visit('/')
+  })
+
+
   it('When I enter "Alien BR" in the title field', () => {})
   it('Then I should see a check icon in the title field', () => {})
   it('When I enter "https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg" in the URL field', () => {})
